cc866e45ab8348e81b4e07f99f61567f7e04c463
zesionlee <39211025+zesion21@users.noreply.github.com>
2025-07-25 15:02:15 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# 确保脚本抛出遇到的错误
+set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
+
+# deploy to github
+msg='来自github action的自动部署'
+githubUrl=https://zesion21:${GITHUB_TOKEN}@github.com/zesion21/zesion21.github.io.git
+git config --global user.name "zesion21"
+git config --global user.email "39211025+zesion21@users.noreply.github.com"
+
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+cd -
+rm -rf docs/.vuepress/dist
